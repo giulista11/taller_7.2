b024834ba023df8f418ebfed7df8cb579e4945d9
giulista11 <giulista11@gmail.com>
2025-08-11 19:40:20 -0300

    actualizacion 3

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
 
-document.getElementById("boton").addEventListener("click", hola);
 
-function hola(){
-    alert("Hola, soy el div,");
-}
 
-function saludar() {
+const boton = document.getElementById("saludar");
+const div = document.getElementById("boton");
+
+boton.addEventListener("click", function(event) {
+    event.stopPropagation(); // Evita que el clic se propague al div
     alert("Holaa");
-}
+});
 
+div.addEventListener("click", function() {
+    alert("Hola, soy el div.");
+});
